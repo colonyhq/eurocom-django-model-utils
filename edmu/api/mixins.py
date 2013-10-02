59d3bacd61aa3791ec877e12518145212a18f633
Christopher Penkin <chris@eurocom.co.za>
2013-10-02 08:53:25 +0200

    Added api utilities for Colony.

---
+++ edmu/api/mixins.py
@@ -0,0 +1,91 @@
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView
+from rest_framework.settings import api_settings
+
+
+class MethodPermissionCheckAPIView(GenericAPIView):
+    """
+    Adds extra methods to the ``GenericAPIView`` class to handle the getting and checking of method permissons.
+    """
+    def get_method_permissions(self, permission_classes):
+        """
+        Instantiates and returns the list of permissions that the calling method requires.
+
+        :param permission_classes:
+            A list of permission classes to instantiate.
+
+        :return:
+            A list of instantiated permission classes.
+        """
+        return [permission() for permission in permission_classes]
+
+    def check_method_permissions(self, permission_classes, request):
+        """
+        Check if the request should be permitted. Raises an appropriate exception if the request is not permitted.
+
+        :param permission_classes:
+            A list of permission classes to check against.
+        """
+        for permission in self.get_method_permissions(permission_classes):
+            if not permission.has_permission(request, self):
+                self.permission_denied(request)
+
+
+class CreateListAPIView(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        MethodPermissionCheckAPIView):
+    """
+    Concrete view for creating, retrieving or listing a model instance.
+    """
+    get_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+    post_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+
+    def get(self, request, *args, **kwargs):
+        self.check_method_permissions(self.get_permission_classes, request)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.check_method_permissions(self.post_permission_classes, request)
+        return self.create(request, *args, **kwargs)
+
+
+class RetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
+                                   mixins.UpdateModelMixin,
+                                   mixins.DestroyModelMixin,
+                                   MethodPermissionCheckAPIView):
+    """
+    Concrete view for retrieving, updating or destroying a model instance.
+    """
+    retrieve_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+    update_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+    destroy_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+
+    def get(self, request, *args, **kwargs):
+        self.check_method_permissions(self.retrieve_permission_classes, request)
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        self.check_method_permissions(self.update_permission_classes, request)
+        return self.update(request, args, kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.check_method_permissions(self.destroy_permission_classes, request)
+        return self.destroy(request, args, kwargs)
+
+
+class RetrieveDestroyAPIView(mixins.RetrieveModelMixin,
+                             mixins.DestroyModelMixin,
+                             MethodPermissionCheckAPIView):
+    """
+    Concrete view for retrieving or destroying a model instance.
+    """
+    retrieve_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+    destroy_permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+
+    def get(self, request, *args, **kwargs):
+        self.check_method_permissions(self.retrieve_permission_classes, request)
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        self.check_method_permissions(self.destroy_permission_classes, request)
+        return self.destroy(request, args, kwargs)
---
+++ edmu/api/permissions.py
@@ -0,0 +1,44 @@
+from rest_framework import permissions
+
+
+class IsStaff(permissions.BasePermission):
+    """
+    Object-level permission, only users that are set as staff members.
+    """
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.owner == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to view or
+    edit it. Assumes the model instance has an `owner` attribute.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Instance must have an attribute named `owner`.
+        return obj.owner == request.user
+
+
+class HasRole(permissions.BasePermission):
+    """
+    Object-level permission only to allow users that has a certain
+    role access to a resource.
+    """
+    def has_permission(self, request, view):
+        # `View` must have an attribute named `_role`
+        return view.role in request.user.roles
