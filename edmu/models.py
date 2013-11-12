21d207abf2fa35b132baf536283d10959fa14cf4
chrispenkin <chris@eurocom.co.za>
2013-11-12 13:17:16 +0200

    COL-85 #close Added the base of Campaign Addons. Super simple design.

--- edmu/models.py
+++ edmu/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Max
 from django.utils.translation import ugettext_lazy as _
 
 from uuidfield import UUIDField
@@ -51,3 +52,53 @@ class UserTimeStampedModel(TimeStampedModel, UserStampedModel):
     """
     class Meta:
         abstract = True
+
+
+class OrderedModel(models.Model):
+    """
+    An abstract model that allows objects to be ordered relative to each other.
+    Provides an ``order`` field.
+    """
+    order = models.PositiveIntegerField(editable=False, db_index=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('order',)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            c = self.__class__.objects.all().aggregate(Max('order')).get('order__max')
+            self.order = c and c + 1 or 0
+        super(OrderedModel, self).save(*args, **kwargs)
+
+    def _move(self, up, qs=None):
+        if qs is None:
+            qs = self.__class__._default_manager
+
+        if up:
+            qs = qs.order_by('-order').filter(order__lt=self.order)
+        else:
+            qs = qs.filter(order__gt=self.order)
+        try:
+            replacement = qs[0]
+        except IndexError:
+            # already first/last
+            return
+        self.order, replacement.order = replacement.order, self.order
+        self.save()
+        replacement.save()
+
+    def move(self, direction, qs=None):
+        self._move(direction == 'up', qs)
+
+    def move_down(self):
+        """
+        Move this object down one position.
+        """
+        return self._move(up=False)
+
+    def move_up(self):
+        """
+        Move this object up one position.
+        """
+        return self._move(up=True)
