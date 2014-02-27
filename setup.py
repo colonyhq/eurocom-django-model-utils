ed6617f4a5f8f0eb89276c144d3ac12bca3c0546
Christopher Penkin <chris.penkin@gmail.com>
2014-02-27 11:39:56 +0200

    Removed the OrderModel and updated the time stamped Model to not be editable.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = '0.3.4'
+version = '0.3.5'
 
 setup(
     name='edmu',
